0848e8e80f05ce3bf03a4b579310cca20072c08f
Felipe Pereira <felipejiti@hotmail.com>
2023-09-27 16:45:26 -0300

    Project created

---
+++ src/test/java/com/lojadesuplementos/api/LojaDeSuplementosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lojadesuplementos.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaDeSuplementosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
